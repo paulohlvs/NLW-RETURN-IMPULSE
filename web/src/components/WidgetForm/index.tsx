9a3de2df41cd182eebe22c6ca8434b773563c4d3
Paulo Henrique <paulo.hlvs@gmail.com>
2022-05-04 00:40:43 -0300

    aula - 02 1:16
    falta revisar textarea

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,60 @@
+import { CloseButton } from "../CloseButton"
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideiaImageUrl from "../../assets/ideia.svg";
+import outroImageUrl from "../../assets/outro.svg"
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+    BUG: {
+        title: "Problema",
+        image: {
+            source: bugImageUrl,
+            alt: "Imagem de um inseto"
+        }
+    },
+    IDEA: {
+        title: "Idéia",
+        image: {
+            source: ideiaImageUrl,
+            alt: "Imagem de uma lâmpada"
+        }
+    },
+    OTHER: {
+        title: "Outro",
+        image: {
+            source: outroImageUrl,
+            alt: "Imagem de uma nuvem de pensamento"
+        }
+    }
+
+}
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function handleRestartfeedbac(){
+        setFeedbackType(null);
+    }
+
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+            ) : (
+                <FeedbackContentStep 
+                feedbackType={feedbackType}
+                onRestartFeedbackRequest={handleRestartfeedbac}/>
+            )
+            }
+            <footer className="text-xs text-neutral-400">Feito com ♥ pela <a href="https://github.com/paulohlvs" target="_blank">Thinness | SI</a></footer>
+        </div >
+    )
+}
