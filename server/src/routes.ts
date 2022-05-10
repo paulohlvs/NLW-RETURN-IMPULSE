9f7052049c74b098689fc3d50ffbe8091435668b
Paulo Henrique <paulo.hlvs@gmail.com>
2022-05-10 20:15:44 -0300

    dependency inversion

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import nodemailer from 'nodemailer'
 import { prisma } from './prisma';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router()
 
@@ -17,18 +19,24 @@ const transport = nodemailer.createTransport({
 routes.post('/feedbacks', async (req, res) => {
 
     const { type, comment, screenshot } = req.body
-    const feedback = 
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+    const submitFeedbacksUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository)
 
-    await transport.sendMail({
-        from: 'equipe feedbacks <feedback@thinness.com.br> ',
-        to: 'Paulo Henrique <paulo@thinness.com.br>',
-        subject: 'Novo Feedback',
-        html: [
-            '<div style="font-family: sans-serife; font-size: 16px; color: #111">',
-            `<p>Tipo de Feedback: ${type}</p>`,
-            `<p>Comentario: ${comment}</p>`,
-        ].join('\n')
+    await submitFeedbacksUseCase.execute({
+        type,
+         comment, 
+        screenshot
     })
+    // await transport.sendMail({
+    //     from: 'equipe feedbacks <feedback@thinness.com.br> ',
+    //     to: 'Paulo Henrique <paulo@thinness.com.br>',
+    //     subject: 'Novo Feedback',
+    //     html: [
+    //         '<div style="font-family: sans-serife; font-size: 16px; color: #111">',
+    //         `<p>Tipo de Feedback: ${type}</p>`,
+    //         `<p>Comentario: ${comment}</p>`,
+    //     ].join('\n')
+    // })
 
-    return res.status(201).json({ data: feedback })
+    return res.status(201).send()
 })
